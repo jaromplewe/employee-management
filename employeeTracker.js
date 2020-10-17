70ca509ceb82eb006c9983b9b0d1671ed6029116
Jarom Plewe <jaromplewe@Jaroms-MacBook-Air-2.local>
2020-10-16 20:30:55 -0600

    added inquirer

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var password = require("./password")
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -18,5 +19,27 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
+  runQuestions();
 });
 
+function runQuestions() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View all employees",
+        "Add employee",
+        "Update employee",
+        "Update employee role",
+        "Add department",
+        "View departments",
+        "Add role",
+        "View roles"
+      ]
+    })
+    .then(function(answer) {
+      console.log("answer: " + JSON.stringify(answer));
+    });
+}
